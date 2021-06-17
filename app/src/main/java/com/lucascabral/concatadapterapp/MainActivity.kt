a733f05d9ffa4da5c9f379945b1a1e4b63e29ac6
Lucas Cabral <lucas_cabral.deve@outlook.com>
2021-06-17 15:04:47 -0300

    Projeto configurado.

--- app/src/main/java/com/lucascabral/concatadapterapp/MainActivity.kt
+++ app/src/main/java/com/lucascabral/concatadapterapp/MainActivity.kt
@@ -2,10 +2,15 @@ package com.lucascabral.concatadapterapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lucascabral.concatadapterapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
