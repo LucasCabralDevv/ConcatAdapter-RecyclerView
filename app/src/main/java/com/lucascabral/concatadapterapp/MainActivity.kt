a61b40b86814c5c11813b7a8b5a9cbb1aff3d73b
Lucas Cabral <lucas_cabral.deve@outlook.com>
2021-06-17 17:16:16 -0300

    Projeto finalizado.

--- app/src/main/java/com/lucascabral/concatadapterapp/MainActivity.kt
+++ app/src/main/java/com/lucascabral/concatadapterapp/MainActivity.kt
@@ -2,15 +2,41 @@ package com.lucascabral.concatadapterapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.ConcatAdapter
+import com.lucascabral.concatadapterapp.adapter.ProgrammingLanguagesAdapter
+import com.lucascabral.concatadapterapp.adapter.TipsAdapter
 import com.lucascabral.concatadapterapp.databinding.ActivityMainBinding
+import com.lucascabral.concatadapterapp.model.programmingLanguages
+import com.lucascabral.concatadapterapp.model.tips
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var programmingLanguagesAdapter: ProgrammingLanguagesAdapter
+    private lateinit var tipsAdapter: TipsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        programmingLanguagesAdapter = ProgrammingLanguagesAdapter()
+        tipsAdapter = TipsAdapter().apply {
+            gotItItemClickListener = {
+                dismissTip()
+            }
+        }
+        binding.mainRecyclerView.adapter = ConcatAdapter(tipsAdapter, programmingLanguagesAdapter)
+
+        tipsAdapter.submitList(tips)
+        programmingLanguagesAdapter.submitList(programmingLanguages)
+    }
+
+    private fun dismissTip() {
+        tipsAdapter.notifyItemRemoved(0)
     }
 }
