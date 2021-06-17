e5e61cd124ba3d16a85d12a47e6cdcdba52c8568
Lucas Cabral <lucas_cabral.deve@outlook.com>
2021-06-17 16:47:33 -0300

    Callback criado para o listener de click do textGotIt.

--- app/src/main/java/com/lucascabral/concatadapterapp/adapter/TipsAdapter.kt
+++ app/src/main/java/com/lucascabral/concatadapterapp/adapter/TipsAdapter.kt
@@ -10,8 +10,10 @@ import com.lucascabral.concatadapterapp.model.Tip
 
 class TipsAdapter : ListAdapter<Tip, TipsAdapter.TipsViewHolder>(DIFF_CALLBACK) {
 
+    var gotItItemClickListener: (() -> Unit)? = null
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TipsViewHolder {
-        return TipsViewHolder.create(parent)
+        return TipsViewHolder.create(parent, gotItItemClickListener)
     }
 
     override fun onBindViewHolder(holder: TipsViewHolder, position: Int) {
@@ -32,20 +34,28 @@ class TipsAdapter : ListAdapter<Tip, TipsAdapter.TipsViewHolder>(DIFF_CALLBACK) 
     }
 
     class TipsViewHolder(
-        private val binding: ItemTipBinding
+        private val binding: ItemTipBinding,
+        private val gotItItemClickListener: (() -> Unit)?
     ) : RecyclerView.ViewHolder(binding.root) {
 
         fun bind(tip: Tip) {
             binding.run {
                 textTip.text = tip.description
             }
+
+            binding.textGotIt.setOnClickListener {
+                gotItItemClickListener?.invoke()
+            }
         }
 
         companion object {
-            fun create(parent: ViewGroup): TipsViewHolder {
+            fun create(
+                parent: ViewGroup,
+                gotItItemClickListener: (() -> Unit)?
+            ): TipsViewHolder {
                 val binding = ItemTipBinding
                     .inflate(LayoutInflater.from(parent.context), parent, false)
-                return TipsViewHolder(binding)
+                return TipsViewHolder(binding, gotItItemClickListener)
             }
         }
     }
